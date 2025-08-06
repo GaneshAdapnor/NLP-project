c1b6d1a8cd6a486b0f52ef8def46a34582e760c3
GaneshAdapnor <162539731+GaneshAdapnor@users.noreply.github.com>
2025-08-06 12:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ with st.sidebar:
         🎲 Random Surprise awaits! 
     """)
     st.markdown("---")
-    st.info("Made with ❤️ by ganesh the Savage 👸")
+    st.info("Made by Ganesh😉")
 
 # 🧠 Main Title
 st.title("💣 Roastify Me")
@@ -189,4 +189,4 @@ if st.session_state.roast_history:
 
 # 👣 Footer
 st.markdown("---")
-st.caption("Made with fire & love 🔥💖 by ganesh")
+st.caption("Made by Ganesh😉")
